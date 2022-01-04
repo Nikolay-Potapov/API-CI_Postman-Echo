6d6f3f1e0573645bdda7afdc0a899db54d3a8be7
Nikolay Potapov <potapov4444gmail.ru>
2022-01-04 17:37:35 +0700

    broke_the_test

--- src/test/java/rest/PostmanTest.java
+++ src/test/java/rest/PostmanTest.java
@@ -14,7 +14,7 @@ class PostmanTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("newData") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
